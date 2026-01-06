41daa6d6673039c19addf121117324e7c8a003ec
ChemzRegaton <112966149+Jedlouies@users.noreply.github.com>
2026-01-06 21:35:43 +0800

    Practice

--- api-gateway/app.jsx
+++ api-gateway/app.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
+import NavBar from "../src/common/nav-bar";
+
 import LandingPage from "./landing-page";
 
 function App() {
     return (
         <div className="app-container">
+            <NavBar />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
 
--- api-gateway/landing-page.jsx
+++ api-gateway/landing-page.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import "./styles/landing-page.css";
 
 function LandingPage() {
+  const bannerText = "Summer Sale is Here! ◌ Get Up to 50% Off on Select Styles! ◌ Limited Time Offer! ◌ ";
+
   return (
-    <div className='landing-page-background'>
-        <h1>Welcome to Rave Shop!</h1>
-        <p>Your one-stop shop for all things rave.</p>
+  <>
+   
+    <div className='landing-page-container'>
+        <div className="sale-container">
+            <div className="moving-text">
+                {bannerText.repeat(120)}
+            </div>
+        </div>
+        <div className="featured-shoe-container">
+            <img className='featured-shoe' src="/Shoes.png" alt="featured-shoes" />
+        </div>
+          <div className="product-featured-container">
+            <h1>Violet Velocity</h1>
+            <h2>Rave Sneakers</h2>
+            <p>Experience motion redefined. The Violet Velocity combines futuristic style with athletic performance. Featuring our signature 'Aero-Mesh' knit for maximum airflow and a dynamic, energy-returning sole that glows with electric purple intensity. Perfect for the night runner looking to make a statement.</p>
+            <p><b>Price:</b> $129.99 <strike>$129.99</strike></p>
+            <button className="buy-now-button">Buy Now</button>
+        </div>
+        <div className="footer">
+            <div className="footer-body">
+                <div className="footer-item">
+                    <img src="/Rave-Logo.png" alt="" width={50}/>
+                    <p>Your ultimate destination for trendy and affordable footwear. Step into style with Rave Shop!</p>
+                </div>
+                <div className="footer-item">
+                    <h3>Policy and Terms</h3>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
+                    <p>Return Policy</p>
+                </div>
+                <div className="footer-item">
+                    <h3>Contact Us</h3>
+                    <p>Email: support@rave.ph</p>
+                    <p>Phone: +1 (555) 123-4567</p>
+                </div>
+            </div>
+        </div>
+
     </div>
+  </>
   );
 }
 
