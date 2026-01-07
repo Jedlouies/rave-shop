bcdbd166a7edf5e411068a44cf6355694cd13169
ChemzRegaton <112966149+Jedlouies@users.noreply.github.com>
2026-01-07 23:52:06 +0800

    update

--- api-gateway/app.jsx
+++ api-gateway/app.jsx
@@ -1,18 +1,25 @@
+
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
 import NavBar from "../src/common/nav-bar";
-
 import LandingPage from "./landing-page";
+import Login from "../src/common/login";
 
 function App() {
+
+    const [showLogin, setShowLogin] = useState(false);
+    
     return (
         <div className="app-container">
-            <NavBar />
+            <NavBar handleLoginClick={() => setShowLogin(true)}/>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-
                 
             </Routes>
+            
+            {showLogin && (
+                <Login onClose={() => setShowLogin(false)} />
+            )}
 
         </div>
     )
